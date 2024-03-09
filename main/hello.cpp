d67c325313cb20ed5f8c301568856766035622ee
sreejasahithi <115860222+sreejasahithi@users.noreply.github.com>
2024-03-09 20:40:20 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,6 +8,6 @@ int main(void) {
   cout << "I have successfully built and run\n";
   cout<<"PES1UG21CS940"<<"\n";
   cout<<"Sreeja Sahithi Chintalapati"<<"\n";
-  cout<"sreeja PES1UG21CS940"<"\n";
+  cout<<"sreeja PES1UG21CS940"<<"\n";
   return 0;
 }
